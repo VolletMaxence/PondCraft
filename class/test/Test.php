a3c003fdf4fe31adaa564488c253b160ba3e04e8
MrRapidecho <jlanglace@la-providence.net>
2021-04-03 16:27:01 +0200

    test unitaire item

---
+++ class/test/Test.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="main.js"></script>
+    <title>Document</title>
+</head>
+<body class="bodyAccueil">
+    <?php
+    //c'est dans fonction que l'on gère les formulaires de Co et les sessions
+
+    include "../../session.php"; 
+    if($access){
+        $access = $Joueur1->deconnectToi();
+    }
+    if($access){
+
+        echo "<div> TEST ITEM";
+            include "testUnitaire/testItem.php"; 
+        echo "</div>" ;
+
+
+    }else{
+        echo $errorMessage;
+    }
+    ?>
+</body>
+</html>
+
+    
--- class/test/testUnitaire/testItem.php
+++ class/test/testUnitaire/testItem.php
@@ -1,28 +1,11 @@
-<?php 
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="main.js"></script>
-    <title>Document</title>
-</head>
-<body class="bodyAccueil">
-    <?php
-    //c'est dans fonction que l'on gère les formulaires de Co et les sessions
 
-    include "../../session.php"; 
-    if($access){
-        $access = $Joueur1->deconnectToi();
-    }
-    if($access){
+<?php
 
         $newItem = new Item($mabase);
 
-        echo "<div><p>Test Creation Item de Soin </p>";
+
+
+        echo "<div><p>Test 1 Creation Item de Soin </p>";
         $newItem= $newItem->createItemSoinConsommable();
         echo "<p>le nom est : ".$newItem->getNom()."</p>";
         echo "<p>le id est : ".$newItem->getId()."</p>";
@@ -34,10 +17,9 @@ session_start();
         }else{
             echo '<div style="color:red">la suppression a echoué car pas id</div>';
         }
-       
         echo "</div>";
 
-        echo "<div><p>Test Creation Item aléatoire </p>";
+        echo "<div><p>Test 2 Creation Item aléatoire </p>";
         $newItem= $newItem->createItemAleatoire();
         echo "<p>le nom est : ".$newItem->getNom()."</p>";
         echo "<p>le id est : ".$newItem->getId()."</p>";
@@ -48,17 +30,35 @@ session_start();
         }else{
             echo '<div style="color:red">la suppression a echoué car pas id</div>';
         }
+        echo "</div>";
+
+
+
+        echo "<div><p>Test 3 100 Item aléatoire </p>";
+        $listItems = array();
+        for($i=0;$i<100;$i++){
+            array_push($listItems,$newItem->createItemAleatoire());
+        }
+        
+        if(count($listItems)>0){
+            echo '<div class="left">Items Présent : <div class="divRarete"> Commun - Rare</div><ul class="Item">';
+            foreach ( $listItems as  $Item) {
+                ?>
+                <li id="item<?php echo $Item->getId()?>" style="<?php echo $Item->getClassRarete()?>">
+                    <a onclick="CallApiAddItemInSac(<?php echo $Item->getId()?>)">
+                        <?php echo $Item->getNom() ?></a>
+                </li>
+                <?php 
+                $Item->deleteItem($Item->getId());
+            }
+            echo '</ul></div>';
+        }
        
         echo "</div>";
+
+?>
         
         
         
 
 
-
-    }else{
-        echo $errorMessage;
-    }
-    ?>
-</body>
-</html>
