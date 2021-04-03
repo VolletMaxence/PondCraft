5807062f20279189fefa71640d70b3447c9102ce
MrRapidecho <jlanglace@la-providence.net>
2021-04-04 00:34:15 +0200

    amelioration image

--- class/Map.php
+++ class/Map.php
@@ -866,7 +866,7 @@ class map{
         if($result=='https://status.unsplash.com/' || $result=='https://images.unsplash.com/source-404'){
             $result='https://i0.wp.com/supertrampontheroad.com/wp-content/uploads/2017/02/DSC_3793-1-2.jpg?resize=1024%2C678&ssl=1';
         }else {
-            $result.="?w=800";
+            $result.="?w=600";
         }
         return  $result;  
     }
