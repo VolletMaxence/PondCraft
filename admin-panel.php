32569a7c0a22016276c6fa209ea7459e0d5ac4e1
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-04-06 18:34:09 +0200

    Céation de Page Admin.

---
+++ admin-panel.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <!-- Compatible / UTF / Viewport-->
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Style CSS / Script -->
+            <link rel="stylesheet" href="css/style.css">
+            <link rel="stylesheet" href="css/index.css">
+            <script src="main.js"></script>
+        <!-- Informations Générales -->
+            <title>Projet Full Stack - Panel Admins</title>
+            <meta name='description' content='Projet Full Stack - Panel Admin'>
+            <meta name='author' content='La Providence - Amiens'>
+            <link rel='shortcut icon' href='favicon.ico'>
+        <!-- Intégration Facebook -->
+            <meta property='og:title' content='Projet Full Stack - Panel Admin'>
+            <meta property='og:description' content='Projet Full Stack - Panel Admin'>
+            <meta property='og:image' content='favicon.ico'>
+        <!-- Intégration Twitter -->
+            <meta name='twitter:title' content='Projet Full Stack - Panel Admin'>
+            <meta name='twitter:description' content='Projet Full Stack - Panel Admin'>
+            <meta name='twitter:image' content='favicon.ico'>
+    </head>
+    <body class="bodyAccueil">
+        <?php
+        include "session.php";
+
+        if($access){
+            $access = $Joueur1->DeconnectToi();
+        }
+        if($access){
+            ?>
+
+            <?php
+        }else{
+            echo $errorMessage;
+        }
+        ?>
+    </body>
+</html>
