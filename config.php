9d9142285557b2e9917dfbb0e9a35e925971dbc2
MrRapidecho <jlanglace@la-providence.net>
2021-04-07 19:22:32 +0200

    Add Bdd

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
-//purge de base 
+//purge de base en LOCAL SEULEMENT !!!
 /*
 DELETE  from map where id <> 0 ;
-UPDATE `map` SET `mapNord`= NULL,`mapSud`=NULL,`mapEst`=NULL,`mapOuest`=NULL WHERE id = 0
+UPDATE `map` SET `mapNord`= NULL,`mapSud`=NULL,`mapEst`=NULL,`mapOuest`=NULL WHERE id = 0;
+UPDATE `Entite` SET `idMap`= 0;
 DELETE  from AttaquePersoMob where 1;
 DELETE  from Entite where type = 2;
 DELETE  from EntiteEquipement where 1;
